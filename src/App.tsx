a3fa123564703753930a5328313e92077e23d4d9
Illia Kozliuk <ikozluk160@gmail.com>
2024-12-15 17:53:26 +0200

    create basic UI for StoryItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { StoryItem } from "./features/stories/components/story-item";
 import { MainPageLayout } from "./layouts/main-page";
 
 function App() {
   return (
     <MainPageLayout>
-      <p>Hello world</p>
+      <StoryItem
+        index={0}
+        title={"bla bla made somthhing incredible..."}
+        url={"https://chatgpt.com/c/675ac06a-ae4c-8009-b33e-8b5ac9dbfd43"}
+        pointCount={3}
+        commentCount={2}
+        creatorName={"bobyTheWitcher"}
+        time={""}
+      />
     </MainPageLayout>
   );
 }
---
+++ src/features/stories/components/story-item.tsx
@@ -0,0 +1,45 @@
+type StoryItemProps = {
+  index: number;
+  title: string;
+  url: string;
+  pointCount: number;
+  creatorName: string;
+  time: string;
+  commentCount: number;
+};
+
+export function StoryItem(props: StoryItemProps) {
+  return (
+    <div className="flex gap-1">
+      <p className="text-gray-400">{props.index}.</p>
+      <button className="flex pt-2">
+        <img width={10} height={10} src="/upvote-arrow.svg" />
+      </button>
+      <div className="flex flex-col text-gray-400 text-sm">
+        <div className="flex">
+          <a href={props.url} className="text-black">
+            {props.title}
+          </a>
+          (
+          <a href="#" className="hover:underline">
+            {new URL(props.url).hostname}
+          </a>
+          )
+        </div>
+        <p className="text-xs">
+          {props.pointCount + " points by "}
+          <a className="hover:underline" href="#">
+            {props.creatorName}
+          </a>
+          <a href="#" className="hover:underline">
+            {props.time} hours ago
+          </a>
+          {" | "}
+          <a href="#" className="hover:underline">
+            {props.commentCount} comments
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
